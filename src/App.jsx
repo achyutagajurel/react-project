c5271db78e7880ff1b5e4cf2a9d53d081f731a41
achyutagajurel <achyutagajurel@gmail.com>
2025-07-09 10:38:05 +0545

    formatting-layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function App() 
+
+
+{
   const [count, setCount] = useState(0)
 
   return (
@@ -30,6 +33,8 @@ function App() {
       </p>
     </>
   )
+
+
 }
 
 export default App
